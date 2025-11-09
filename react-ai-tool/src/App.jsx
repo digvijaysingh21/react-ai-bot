8c7c25c12c817d1674219de06cb8a6472657453e
digvijaysingh21 <singh.v.digvijay@gmail.com>
2025-11-09 10:22:50 +0530

    initial setup

---
+++ react-ai-tool/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* left side bar */}
+      <div></div>
+
+      {/* main area */}
+    </div>
+  );
+}
+
+export default App;
