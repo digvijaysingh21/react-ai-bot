091f28e23cb5617b002f529a824aeb01f348bc90
digvijaysingh21 <singh.v.digvijay@gmail.com>
2025-11-09 11:31:07 +0530

    chat-ui

--- react-ai-tool/src/App.jsx
+++ react-ai-tool/src/App.jsx
@@ -3,11 +3,23 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="grid grid-cols-5">
       {/* left side bar */}
-      <div></div>
+      <div className="col-span-1 bg-zinc-800 h-screen text-center"></div>
 
       {/* main area */}
+      <div className="col-span-4 p-10">
+        <div className="container h-130"></div>
+
+        <div className="bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16">
+          <input
+            type="text"
+            className="w-full h-full p-3 outline-none"
+            placeholder="Ask me anything"
+          />
+          <button>Ask</button>
+        </div>
+      </div>
     </div>
   );
 }
